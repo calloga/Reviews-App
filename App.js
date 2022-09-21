9e50fed2f93c36f95fdad03a8fb08a740248d76e
Calloga <papeabdoucalloga98@gmail.com>
2022-09-21 16:21:51 +0000

    Working on the SplashScreen

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import Home from "./screens/Home";
 import { useFonts } from "expo-font";
-import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
+import { Text, View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  let fontsLoaded = useFonts({
+  const [fontsLoaded] = useFonts({
     "Montserrat-Regular": require("./assets/fonts/Montserrat/Montserrat-Regular.ttf"),
     "Montserrat-ExtraBold": require("./assets/fonts/Montserrat/Montserrat-ExtraBold.ttf"),
   });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
   if (!fontsLoaded) {
-    return <AppLoading onError={console.warn} />;
-  } else {
-    return <Home />;
+    console.log("Fonts");
+    return <Text>Loading...</Text>;
   }
+
+  return (
+    <View onLayout={onLayoutRootView}>
+      <Home />
+    </View>
+  );
 }
